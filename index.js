ee5dee6bd14d04d6169b4a0a0c2bd59a7555b996
ayushdwivedee <ayushbhartiya1494927@gmail.com>
2024-07-07 15:04:42 +0530

    File system assignment is completed

---
+++ index.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+ 
+
+const operation = process.argv[2];
+const file = process.argv[3];
+let content = process.argv[4];
+ 
+if (content==undefined) {
+  content = "";
+}
+switch (operation) {
+  // complete the fillowing function.
+  case "read":
+    console.log(
+      `This text is available in your test.txt file : ${fs.readFileSync(file)}`
+    );
+    break;
+  case "append":
+    fs.appendFileSync(file, content);
+    console.log(`This text is appended in your test.txt file : ${content}`);
+    break;
+  case "rename":
+    fs.renameSync(file, content);
+    console.log(`This file is renamed as : ${content}`);
+    break;
+  case "delete":
+    fs.unlinkSync(file);
+    console.log(`This ${file} deleted`);
+    break;
+  case "create":
+    fs.writeFileSync(file, content);
+    console.log(`This ${file} created`)
+    break;
+    case "list":
+        fileObjs = fs.readdirSync(process.cwd(), { withFileTypes: true }); 
+  
+        console.log("\n Current directory files:"); 
+        fileObjs.forEach(file => { 
+          console.log(file); 
+        }); 
+  console.log(`All Files and directory name got printed.`)
+  break;
+  default:
+    console.log(`Invalid operation '${operation}'`);
+}
